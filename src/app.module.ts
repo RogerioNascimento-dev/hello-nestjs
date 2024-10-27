681d4ba7d89c5f623774a8c35170acee51bf5e75
Rogério Nascimento <rogerionascimento.dev@outlook.com.br>
2024-10-27 02:21:39 +0000

    feat: implement config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { envValidator } from './env'
 import { PrismaService } from './prisma/prisma.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envValidator.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -8,15 +8,10 @@ import {
 import { hash } from 'bcryptjs'
 import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
-import { z } from 'zod'
-
-const createAccountRequest = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string().min(6),
-})
-
-type CreateAccountRequest = z.infer<typeof createAccountRequest>
+import {
+  CreateAccountRequest,
+  createAccountRequest,
+} from './validators/create-account-request'
 
 @Controller('/accounts')
 export class CreateAccountController {
