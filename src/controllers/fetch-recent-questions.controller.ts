d8d2dc83775fe0158f025179856e457c3de823b9
Rogério Nascimento <rogerionascimento.dev@outlook.com.br>
2024-10-28 22:23:26 +0000

    feat: implements controller for listing recent questions

---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { PrismaService } from 'src/prisma/prisma.service'
+import {
+  PageQueryParams,
+  pageQueryParamsPipe,
+} from './validators/fetch-recent-questions-request.validator'
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(private prisma: PrismaService) {}
+  @Get()
+  async handler(@Query('page', pageQueryParamsPipe) page: PageQueryParams) {
+    const perPage = 10
+    const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: { createdAt: 'desc' },
+    })
+
+    return { questions }
+  }
+}
---
+++ src/controllers/validators/fetch-recent-questions-request.validator.ts
@@ -0,0 +1,13 @@
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { z } from 'zod'
+
+export const pageQueryParams = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+export type PageQueryParams = z.infer<typeof pageQueryParams>
+
+export const pageQueryParamsPipe = new ZodValidationPipe(pageQueryParams)
