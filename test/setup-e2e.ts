b0a3e0b82f09d358d3529b4f4aca771489572928
Rogério Nascimento <rogerionascimento.dev@outlook.com.br>
2024-10-28 23:26:52 +0000

    feat: implements isolated database creation for each test file

---
+++ test/setup-e2e.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+
+const prisma = new PrismaClient()
+const schemaId = `test-${randomUUID()}`
+function generateUniqueDatabaseUrl(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Pleas provide DATABASE_URL environment variable.')
+  }
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', `${schemaId}`)
+  return url.toString()
+}
+
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseUrl(schemaId)
+  process.env.DATABASE_URL = databaseURL
+  execSync(`npx prisma migrate deploy`)
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+  await prisma.$disconnect()
+})
