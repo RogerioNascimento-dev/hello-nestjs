5b54c1c3f1a766c93528f3e6bc93a9f86e6c610d
Rogério Nascimento <rogerionascimento.dev@outlook.com.br>
2024-11-03 22:29:16 +0000

    feat: implements env module

--- src/infra/auth/jwt.strategy.ts
+++ src/infra/auth/jwt.strategy.ts
@@ -1,14 +1,13 @@
-import { Env } from '@/infra/env'
 import { Injectable } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 import { PassportStrategy } from '@nestjs/passport'
 import { ExtractJwt, Strategy } from 'passport-jwt'
+import { EnvService } from '../env/env.service'
 import { AuthUser, jwtHeaderValidator } from './validators/jwt-header.validator'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService<Env, true>) {
-    const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+  constructor(config: EnvService) {
+    const publicKey = config.get('JWT_PUBLIC_KEY')
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: Buffer.from(publicKey, 'base64'),
