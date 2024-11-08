30e9a1362352882cd7eda4e18f08e9ebdc47375b
Rogério Nascimento <rogerionascimento.dev@outlook.com.br>
2024-11-08 10:54:44 +0000

    feat: create faker class to create database question

--- src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -1,28 +1,29 @@
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 import { AppModule } from '@/infra/app.module'
 import { DatabaseModule } from '@/infra/database/database.module'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
+import { QuestionFactory } from 'test/factories/make-question'
 import { StudentFactory } from 'test/factories/make-student'
 
 describe('Get question by slug (E2E)', () => {
   let app: INestApplication
-  let prisma: PrismaService
   let jwt: JwtService
   let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory],
+      providers: [StudentFactory, QuestionFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
-    prisma = moduleRef.get(PrismaService)
     jwt = moduleRef.get(JwtService)
     studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
     await app.init()
   })
 
@@ -31,23 +32,20 @@ describe('Get question by slug (E2E)', () => {
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
-    await prisma.question.create({
-      data: {
-        title: 'Question 1?',
-        content: 'This is a question 1.',
-        authorId: user.id.toString(),
-        slug: 'this-is-a-question-1',
-      },
+    await questionFactory.makePrismaQuestion({
+      title: 'How to test an API?',
+      slug: Slug.create('how-to-test-an-api'),
+      authorId: user.id,
     })
 
     const response = await request(app.getHttpServer())
-      .get('/questions/this-is-a-question-1')
+      .get(`/questions/how-to-test-an-api`)
       .set('Authorization', `Bearer ${accessToken}`)
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual(
       expect.objectContaining({
-        question: expect.objectContaining({ title: 'Question 1?' }),
+        question: expect.objectContaining({ title: 'How to test an API?' }),
       }),
     )
   })
--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,7 +3,10 @@ import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
+import { PrismaQuestionMapper } from '@/infra/database/mappers/prisma-question-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeQuestion(
   override: Partial<QuestionProps> = {},
@@ -39,4 +42,21 @@ export function makeManyQuestions(
     )
   }
   return questions
+}
+
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestion(
+    data: Partial<QuestionProps> = {},
+  ): Promise<Question> {
+    const question = makeQuestion(data)
+
+    await this.prisma.question.create({
+      data: PrismaQuestionMapper.toPrisma(question),
+    })
+
+    return question
+  }
 }
